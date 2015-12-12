7903b7090cd531570e03df28d030c9ddbfb952a6
Artem <namitos@users.noreply.github.com>
2015-12-12 17:30:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,92 @@
 'use strict';
 
 var revalidator = require('revalidator');
+var _ = require('lodash');
 
-module.exports = function (app) {
-	return class Model {
-		constructor(properties) {
-			var model = this;
-			Object.keys(properties).forEach(function (prop) {
-				model[prop] = properties[prop];
+class Collection extends Array {
+	/**
+	 *
+	 * @param items
+	 * @param Model
+	 * @returns {Collection}
+	 */
+	wrap(items, Model) {
+		if (items && items.length) {
+			items.forEach((item) => {
+				if (item instanceof Model) {
+					this.push(item);
+				} else {
+					this.push(new Model(item));
+				}
 			});
-			if (model._id) {
-				model._id = app.util.prepareId(model._id);
+		}
+		return this;
+	}
+
+	/**
+	 *
+	 * lodash wrapper
+	 * @returns {*}
+	 */
+	get _() {
+		return _(this);
+	}
+}
+
+module.exports = (app) => {
+	return class Model {
+		/**
+		 *
+		 * @param properties
+		 */
+		constructor(properties) {
+			Object.keys(properties).forEach((prop) => {
+				this[prop] = properties[prop];
+			});
+			if (this._id) {
+				this._id = app.util.prepareId(this._id);
 			}
 		}
 
+		/**
+		 * useful wrapper for getting values of deep objects
+		 * @param path
+		 * @returns {*}
+		 */
+		get(path) {
+			return _.get(this, path);
+		}
+
+		/**
+		 * useful wrapper for setting values of deep objects
+		 * @param path
+		 * @returns {Object}
+		 */
+		set(path) {
+			return _.set(this, path);
+		}
+
+		/**
+		 *
+		 * @returns {{Object}}
+		 */
 		toJSON() {
-			var model = this;
 			var result = {};
-			Object.keys(this).forEach(function (prop) {
-				result[prop] = model[prop];
+			Object.keys(this).forEach((prop) => {
+				result[prop] = this[prop];
 			});
 			return result;
 		}
 
+		/**
+		 * prepares the object to database
+		 * @param op
+		 * @returns {Promise}
+		 */
 		prepare(op) {
-			var model = this;
-			return new Promise(function (resolve, reject) {
-				var data = app.util.forceSchema(model.constructor.schema, model);
-				var validation = revalidator.validate(data, model.constructor.schema);
+			return new Promise((resolve, reject) => {
+				var data = app.util.forceSchema(this.constructor.schema, this);
+				var validation = revalidator.validate(data, this.constructor.schema);
 				if (validation.valid) {
 					resolve(data);
 				} else {
@@ -39,69 +98,102 @@ module.exports = function (app) {
 			});
 		}
 
+		/**
+		 *
+		 * @returns {Promise}
+		 */
 		create() {
-			var model = this;
-			return model.prepare('create').then(function (data) {
-				return app.db.collection(model.constructor.schema.name).insertOne(data);
-			}).then(function (result) {
-				return new Promise(function (resolve) {
-					model._id = result.ops[0]._id;
-					resolve(model);
-				});
+			return this.prepare('create').then((data) => {
+				return app.db.collection(this.constructor.schema.name).insertOne(data);
+			}).then((result) => {
+				this._id = result.ops[0]._id;
+				return this;
 			});
 		}
 
+		/**
+		 *
+		 * @returns {Promise}
+		 */
 		update() {
-			var model = this;
-			return model.prepare('update').then(function (data) {
-				if (model.constructor.schema.updatePatch) {
+			return this.prepare('update').then((data) => {
+				if (this.constructor.schema.updatePatch) {
 					data = {
 						$set: data
 					};
 				}
-				return app.db.collection(model.constructor.schema.name).updateOne({
-					_id: model._id
-				}, data).then(function () {
-					return new Promise(function (resolve) {
-						resolve(model);
-					});
+				return app.db.collection(this.constructor.schema.name).updateOne({
+					_id: this._id
+				}, data).then(() => {
+					return this;
 				});
 			});
 		}
 
+		/**
+		 *
+		 * @returns {Promise}
+		 */
 		delete() {
-			var model = this;
-			if (model.constructor.schema.safeDelete) {
-				model.deleted = true;
-				return model.update();
+			if (this.constructor.schema.safeDelete) {
+				this.deleted = true;
+				return this.update();
 			} else {
-				return app.db.collection(model.constructor.schema.name).deleteOne({
-					_id: model._id
+				return app.db.collection(this.constructor.schema.name).deleteOne({
+					_id: this._id
 				});
 			}
 		}
 
+		/**
+		 *
+		 * @returns {Collection}
+		 * @constructor
+		 */
+		static get Collection() {
+			return Collection;
+		}
+
+		/**
+		 *
+		 * @param where
+		 * @param options
+		 * @param connections
+		 * @returns {Promise}
+		 */
 		static read(where, options, connections) {
-			var This = this;
-			return new Promise(function (resolve, reject) {
-				//TODO: тут надо делать подтягивание связей
-				where = where || {};
-				options = options || {};
-				app.db.collection(This.schema.name).find(where, options).toArray().then(function (result) {
-					resolve(result.map(function (row) {
-						return new This(row);
-					}));
-				}).catch(function (err) {
-					reject(err);
-				});
+			//TODO join connections D:
+			where = where || {};
+			options = options || {};
+			return app.db.collection(this.schema.name).find(where, options).toArray().then((result) => {
+				return new this.Collection().wrap(result, this);
 			});
 		}
 
+		/**
+		 * find element by id, rejects if not found
+		 * @param id
+		 * @param options
+		 * @param connections
+		 * @returns {Promise}
+		 */
 		static byId(id, options, connections) {
-			return this.read({
-				_id: app.util.prepareId(id)
-			}, options, connections).then(function (result) {
-				return new Promise(function (resolve, reject) {
+			return this.by('_id', app.util.prepareId(id), options, connections);
+		}
+
+		/**
+		 * find one element by {field}, rejects if not found
+		 * @param field
+		 * @param key
+		 * @param options
+		 * @param connections
+		 * @returns {Promise}
+		 */
+		static by(field, key, options, connections) {
+			var where = {};
+			where[field] = key;
+			return this.read(where, options, connections).then((result) => {
+				return new Promise((resolve, reject) => {
 					if (result.length) {
 						resolve(result[0]);
 					} else {
